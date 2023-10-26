e01354a18fae2196cf7bc01f7a61d6541e7224cc
goodeeit <gt_min@naver.com>
2023-10-26 18:58:46 +0900

    20231026

---
+++ 05_react/react-basic/src/component/MyFirstComponent.jsx
@@ -0,0 +1,20 @@
+/*
+  Component
+  1. 화면 구성의 기본 단위
+  2. 함수형 컴포넌트를 주로 사용(함수형, 클래스형)
+*/
+/*
+  jsx
+  1. 자바스크립트 확장 문법
+  2. 자바스크립트 코드 간결화 가능
+*/
+function MyFirstComponent() {
+  return (
+    <div className="my-first-component">
+      <h1>MyFirstComponent</h1>
+      {/* 주석 */}
+    </div>
+  );
+}
+
+export default MyFirstComponent;
